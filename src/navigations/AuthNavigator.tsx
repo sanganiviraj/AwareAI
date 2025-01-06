be9f87ad84b94c3d8083fbaaff6b8c89f4ba1ea8
Sangani Viraj <sanganiviraj263@gmail.com>
2025-01-06 13:08:39 +0530

    change on homescreen

--- src/navigations/AuthNavigator.tsx
+++ src/navigations/AuthNavigator.tsx
@@ -1,4 +1,3 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Authlogin from '../loginflow/Authlogin';
